4b64a8c91e6c9ae1b94549382aaa570e613cfa10
Katherine Korobov <katya.korobov@gmail.com>
2025-03-04 00:06:45 -0700

    cahnges

--- ASEN2804_Aerospace_Design_Model_Main.m
+++ ASEN2804_Aerospace_Design_Model_Main.m
@@ -21,9 +21,9 @@
 % Date Last Change: 6 Feb 2025
 
 %% Clean Workspace and Housekeeping
-clear
-% clearvars
-close all
+clc        % clear command window
+clear      % clearvars
+close all  % close figure
 
 % removes warnings for table variable names for a cleaner output
 warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames')
@@ -104,12 +104,12 @@ Material_Data = readtable(Design_Input_Filename,'Sheet','Materials'); %Read in p
     g = 9.81; %Sets constant acceleration of gravity [m/s]
 
 % Call Wing Geometry Calcuation Function
-    Plot_WingGeo_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 100 - 199)
+    Plot_WingGeo_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 100 - 199)
     WingGeo_Data = WingGeo(Design_Input,Count,Plot_WingGeo_Data); %Calculate specific wing geometry from wing configuration parameters
 
 %% Calculations - Lift and Drag
 % Call Wing Lift & Drag Model Function
-    Plot_Wing_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 200 - 299)
+    Plot_Wing_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 200 - 299)
     [WingLiftModel,AoA,AoA_Count,AirfoilLiftCurve,WingLiftCurve,WingDragCurve] =...
         WingLiftDrag(Design_Input,Airfoil,Count,Benchmark,Plot_Wing_Data); 
 
@@ -119,45 +119,45 @@ Material_Data = readtable(Design_Input_Filename,'Sheet','Materials'); %Read in p
         ParasiteDrag(Design_Input,Airfoil,WingGeo_Data,ATMOS,Count,Plot_Parasite_Data);
 
 % Call Induced Drag Model Function
-    Plot_Induced_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 400 - 499)
+    Plot_Induced_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 400 - 499)
     InducedDrag_Data = ...
         InducedDrag(Design_Input,WingLiftModel,WingLiftCurve,WingDragCurve,WingGeo_Data,Count,Benchmark,Plot_Induced_Data);
 
 % Call Complete Drag Polar Function
-Plot_DragPolar_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 500 - 599)
+Plot_DragPolar_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 500 - 599)
 [DragPolar_mod1,DragPolar_mod2,DragPolar_mod3] = ...
     DragPolar(Parasite_Drag_Data,InducedDrag_Data,Design_Input,AoA_Count,WingLiftCurve,WingDragCurve,AirfoilLiftCurve,Airfoil,Benchmark,Count,Plot_DragPolar_Data);
 
 % Call L/D Analysis Function
-    Plot_LD_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 600 - 699)
+    Plot_LD_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 600 - 699)
     [LD_mod1,LD_mod2,LD_mod3,LD_benchmark] = ...
         LD(Benchmark,DragPolar_mod1,DragPolar_mod2,DragPolar_mod3,WingLiftCurve,WingDragCurve,AoA_Count,Count,Plot_LD_Data);
 
 % Call Weight Model
-    Plot_Weight_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 700 - 799)
+    Plot_Weight_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 700 - 799)
     [Weight_Data,CG_Data] = ...
         Weight(Design_Input,Count,WingGeo_Data,Airfoil,Material_Data,Component_Data,g,Plot_Weight_Data);
 
 %% Calculations - Dynamic Models
 % Call Thrust Model
-    Plot_Thrust_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 800 - 899)
+    Plot_Thrust_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 800 - 899)
     [ThrustCurves, Time, ThrustStruct] = Thrust(Plot_Thrust_Data);
 
 % Call Boost-Ascent Flight Dynamics Model
-    Plot_Boost_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 900 - 999)
+    Plot_Boost_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 900 - 999)
     [apogee, hApogee, StateStruct] = BoostAscent(Design_Input, ATMOS, Parasite_Drag_Data, Weight_Data, ThrustCurves, Time, Count, g,Plot_Boost_Data);
 
 % Call Glide Flight Dynamics Model (must select one drag polar model L/D
 % data for use in this model)
-    Plot_Glide_Data = 1; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 1000 - 1099)
+    Plot_Glide_Data = 0; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 1000 - 1099)
     LD_Model = LD_mod1; %You must select one LD model output (from the LD function outputs) to utilize for this analysis
     [GlideData] = GlideDescent(LD_Model, apogee, Design_Input, ATMOS, Weight_Data, WingLiftModel, WingLiftCurve,WingDragCurve,hApogee,Count,Plot_Glide_Data); %Must select LD of your best model
 
-% %% Calculations - Stability Model
-% % Call Static Stability Function
-%     Plot_Stability_Data = 01; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 1100 - 1199)
-%     [Boost_Initial_Stab,Boost_75_Stab,Boost_50_Stab,Boost_25_Stab,Boost_Empty_Stab,Glide_Stab,STAB_SM_SUMMARY,STAB_Xcg_SUMMARY,STAB_Xnp_SUMMARY,STAB_Vh_SUMMARY,STAB_Vv_SUMMARY,STAB_GLIDE_h1_SUMMARY]...
-%         = Stability(Design_Input, Count, CG_Data, WingGeo_Data, GlideData, WingLiftModel, Component_Data,Plot_Stability_Data);
+%% Calculations - Stability Model
+% Call Static Stability Function
+    Plot_Stability_Data = 01; %Set to 0 to suppress plots for this function or 1 to output plots (Fig 1100 - 1199)
+    [Boost_Initial_Stab,Boost_75_Stab,Boost_50_Stab,Boost_25_Stab,Boost_Empty_Stab,Glide_Stab,STAB_SM_SUMMARY,STAB_Xcg_SUMMARY,STAB_Xnp_SUMMARY,STAB_Vh_SUMMARY,STAB_Vv_SUMMARY,STAB_GLIDE_h1_SUMMARY]...
+        = Stability(Design_Input, Count, CG_Data, WingGeo_Data, GlideData, WingLiftModel, Component_Data,Plot_Stability_Data);
 %%  Integrated Design and Trade Study Plots (2000 series plots)
     % Merged Boost-Ascent+Glide Flight Profile
     % Some setup to make plots more readable in color, look up the
