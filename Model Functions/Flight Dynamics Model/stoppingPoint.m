d82be2cabd704d39b86111d29b7f6497241abbe5
Katherine Korobov <katya.korobov@gmail.com>
2025-03-12 21:01:46 -0600

    fixed

--- Model Functions/Flight Dynamics Model/stoppingPoint.m
+++ Model Functions/Flight Dynamics Model/stoppingPoint.m
@@ -1,4 +1,4 @@
- function [value, isTerminal, direction] = stoppingPoint(t, S)
+function [value, isTerminal, direction] = stoppingPoint(t, S)
 value = S(6); % This is the variable we want to check (z)
 isTerminal = 1; % tells that we want to stop the integration when
 direction = 1; % the sign goes negative
